c5a12e0f379a5a68d94bdc907edb687d9685bcb2
toanlcgift <toanlcgift@gmail.com>
2021-06-11 13:01:17 +0700

    update

--- XFApp/XFApp/ILWorks/ILWorkRegisters.cs
+++ XFApp/XFApp/ILWorks/ILWorkRegisters.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using XFApp.ILWorks.Adaptors;
+using XFApp.ILWorks.Adaptors.Buttons;
 using XFApp.ILWorks.Adaptors.Pages;
 using XFApp.Services;
 
@@ -9,30 +10,27 @@ namespace XFApp.ILWorks
 {
     public unsafe static class ILWorkRegisters
     {
-        /// <summary>
-        /// 注册委托
-        /// </summary>
-        /// <param name="hotUpdateService"></param>
         public static void RegisterDelegates(HotUpdateService hotUpdateService)
         {
-
+            hotUpdateService.DelegateManager.RegisterMethodDelegate<object, EventArgs>();
+            hotUpdateService.DelegateManager.RegisterMethodDelegate<EventHandler<EventArgs>>();
+            hotUpdateService.DelegateManager.RegisterMethodDelegate<EventArgs>();
+            hotUpdateService.DelegateManager.RegisterDelegateConvertor<EventHandler<EventArgs>>((action) =>
+            {
+                return action;
+            });
         }
 
         internal static void RegisterCLRMethodRedirections(HotUpdateService hotUpdateService)
         {
-            //CLR重定向
-            //Redirect.RedirectXIL.Register(xil);
-            //LitJson.JsonMapper.RegisterILRuntimeCLRRedirection(xil.ILRuntimeAppDomain);
+
         }
 
-        /// <summary>
-        /// 注册跨域适配器
-        /// </summary>
-        /// <param name="hotUpdateService"></param>
         public static void RegisterCrossBindingAdaptors(HotUpdateService hotUpdateService)
         {
             hotUpdateService.RegisterCrossBindingAdaptor(new IAsyncStateMachineClassInheritanceAdaptor()); //用于Async/await
             hotUpdateService.RegisterCrossBindingAdaptor(new ContentPageAdaptors());
+            hotUpdateService.RegisterCrossBindingAdaptor(new ButtonAdaptors());
         }
     }
 }
--- XFApp/XFApp/Services/HotUpdateService.cs
+++ XFApp/XFApp/Services/HotUpdateService.cs
@@ -33,17 +33,12 @@ namespace XFApp.Services
         {
             if (m_Inited) return;
 
-            //注册CLR重定向
             this.registerCLRMethodRedirections();
 
-            //注册委托适配器
             this.registerDelegates();
 
-            //注册跨域适配器
             this.registerCrossBindingAdaptors();
-            //注册CLR绑定
 
-            //加载Assemblies
             await this.LoadAssemblies();
 
             m_Inited = true;
@@ -56,42 +51,29 @@ namespace XFApp.Services
             return this;
         }
 
-        /// <summary>
-        /// 加载热更工程，这里是技术尝试demo，所以特别简单，直接从嵌入资源里加载了
-        /// </summary>
-        /// <returns></returns>
         private async Task LoadAssemblies()
         {
             var asm = IntrospectionExtensions.GetTypeInfo(typeof(HotUpdateService)).Assembly;
             var resources = asm.GetManifestResourceNames();
-            Stream stream_dll = asm.GetManifestResourceStream("XFApp.HotDlls.XFApp.HotUpdate.dll.bytes");
-            Stream stream_pdb = asm.GetManifestResourceStream("XFApp.HotDlls.XFApp.HotUpdate.pdb.bytes");
+            Stream stream_dll = asm.GetManifestResourceStream("XFApp.HotDlls.XFApp.HotUpdate.dll");
+            Stream stream_pdb = asm.GetManifestResourceStream("XFApp.HotDlls.XFApp.HotUpdate.pdb");
 
             m_AppDomain.LoadAssembly(stream_dll, stream_pdb, new PdbReaderProvider());
 
             await Task.CompletedTask;
         }
 
-        /// <summary>
-        /// 注册跨域继承适配器
-        /// </summary>
         private void registerCrossBindingAdaptors()
         {
             ILWorkRegisters.RegisterCrossBindingAdaptors(this);
         }
 
-        /// <summary>
-        /// 注册委托适配器
-        /// </summary>
         private void registerDelegates()
         {
-            //内部委托适配器
+
             ILWorkRegisters.RegisterDelegates(this);
         }
 
-        /// <summary>
-        /// 注册CLR重定向
-        /// </summary>
         private void registerCLRMethodRedirections()
         {
             ILWorkRegisters.RegisterCLRMethodRedirections(this);
